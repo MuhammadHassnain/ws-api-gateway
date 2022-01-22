7b5c164c74a5f0242622f1689e88c02e34fc3f74
MuhammadHassnain <muhammadhassnain776@gmail.com>
2022-01-22 12:40:44 +0500

    Initial Commit.

---
+++ src/main/java/com/hassnain/wsapigateway/WsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hassnain.wsapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hassnain/wsapigateway/WsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hassnain.wsapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
